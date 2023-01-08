df466fc4be461cf8f784fcc0236c5a4b0d4c2803
Bhargavi Thaduri <bhargavithaduri1@gmail.com>
2023-01-08 20:33:45 +0530

    Seeding Campgrounds using default Values in Cities.js and descriptorsAndPlaces.js files

---
+++ seeds/index.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+
+// -------------Requiring the cities file-----
+const cities = require('../seeds/cities');
+
+// ------------Requiring the descriptors and Places file
+const {places,descriptors} = require('../seeds/descriptorsAndplaces');
+
+mongoose.set('strictQuery',true);
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+.then(()=>{
+    console.log('Mongo Connection Open!');
+})
+.catch((err)=>{
+    console.log('Error in Mongo Connection!');
+})
+
+const randomElement = (array)=> array[Math.floor(Math.random()*array.length)]
+
+const seedDB = async ()=>{
+    await Campground.deleteMany({});
+    for(let i=0;i<50;i++){
+        const random1000 = Math.floor(Math.random()*1000);
+        const camp= new Campground({
+            location:`${cities[random1000].city},${cities[random1000].state}`,
+            title:`${randomElement(descriptors)} ${randomElement(places)}`
+        })
+        await camp.save();
+    }
+}
+seedDB();
