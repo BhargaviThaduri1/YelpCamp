830080bbec8de3cd2b625685268662ce7e03dc09
Bhargavi Thaduri <bhargavithaduri1@gmail.com>
2023-01-10 19:47:00 +0530

    Add layouts folder because layout which add the current content as the body to view

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 
@@ -23,6 +25,9 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
 
 app.use(methodOverride('_method'));
+
+// App to use ejsMate engine
+app.engine('ejs',ejsMate)
 
 // Home Route
 app.get('/',(req,res)=>{
