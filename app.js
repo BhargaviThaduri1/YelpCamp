ff49d35f71a48ea9c652c372a484446542df71a3
Bhargavi Thaduri <bhargavithaduri1@gmail.com>
2023-01-22 19:39:48 +0530

    Delete a review route

--- app.js
+++ app.js
@@ -140,6 +140,10 @@ app.delete('/campgrounds/:id', catchAsync(async (req,res)=>{
 }))
 
 
+
+
+// ---------------------Reviews Routes----------------------
+
 // ----------------Reviews post route-----------
 app.post('/campgrounds/:id/reviews',validateReview,catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
@@ -148,6 +152,15 @@ app.post('/campgrounds/:id/reviews',validateReview,catchAsync(async(req,res)=>{
     await review.save();
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+
+// ----------------Delete a review-----------------
+app.delete('/campgrounds/:id/reviews/:reviewId',catchAsync(async(req,res)=>{
+    const {id,reviewId} = req.params;
+    await Campground.findByIdAndUpdate(id,{ $pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
 }))
 
 
