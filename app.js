5118ec955d17dacf77293f86cd2cf222a4d696f0
Bhargavi Thaduri <bhargavithaduri1@gmail.com>
2023-01-28 18:58:22 +0530

    Break out Campground Routes to sepreate file in routes directory which is campground.js

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const methodOverride = require('method-override');
 const ExpressError = require('./errorutlis/ExpressError');
 const catchAsync   = require('./errorutlis/catchAsync');
 
+const campgroundRoute = require('./routes/campground')
+
 
 // Mongoose Connection
 mongoose.set('strictQuery',true);
@@ -45,20 +47,6 @@ Creates reusable code that will meet our goal to reduce duplicating code. like u
 app.engine('ejs',ejsMate)
 
 
-/*alidating the campground before campground is even created using JOI validateCampground is a middleware which will be applied to put and post requests
- campgroundSchema is in the file schemas.js
-*/
-const validateCampground =(req,res,next)=>{ 
-    const {error} = campgroundSchema.validate(req.body);
-    if(error){
-    const msg = error.details.map(el=> el.message).join(',');
-    throw new ExpressError(msg,400);
-    }
-    else{
-        next();
-    }
-}
-
 /*validating the review  before review is even created using JOI validateReview is a middleware which will be applied to put and post requests
  reviewSchema is in the file schemas.js
  This middleware should be applied to post and put request while creating the review
@@ -79,72 +67,7 @@ app.get('/',(req,res)=>{
    res.render('home.ejs');
 })
 
-
-// Route which displays all the campgrounds
-app.get('/campgrounds', catchAsync(async (req,res)=>{
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index',{campgrounds});
-}))
-
-
-// Route to create a new Campground
-app.get('/campgrounds/new',(req,res)=>{
-    res.render('campgrounds/new');
-})
-
-/* Route for the post request in creating the campground and then redirecting to show page 
-/campgrounds/:id--> show the details of campground
-validating the review  before review is even created using validateCampground Middleware which uses JOI
-*/
-app.post('/campgrounds/new',validateCampground,catchAsync(async (req,res,next)=>{
-    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
-    
-    // Validating the req.body using JOI before even creating the campground
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-    
-}))
-
-
-/*
- Route which displays the details of a campground
- And also display the reviews for each campground by populating the reviews
-*/
- app.get('/campgrounds/:id', catchAsync(async(req,res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findById(id).populate('reviews');
-    res.render('campgrounds/show',{campground});
-}))
-
-
-// Route which renders the form to edit a campground
-app.get('/campgrounds/:id/edit', catchAsync(async (req,res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findById(id);
-    res.render('campgrounds/edit',{campground});
-}))
-
-
-// Route which actually update a campground
-app.put('/campgrounds/:id/edit', validateCampground,catchAsync(async (req,res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-
-/* Route which deletes a campground and then redirecting to campgrounds
-   And also deletes all the reviews which are associated to that campground by using
-   campgroundSchema.post('findoneAndDelete',(data)) post middleware in campground model
- */
-app.delete('/campgrounds/:id', catchAsync(async (req,res)=>{
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id)
-    res.redirect('/campgrounds');
-}))
-
-
+app.use('/campgrounds',campgroundRoute)
 
 
 // ---------------------Reviews Routes----------------------
---
+++ routes/campground.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const router = express.Router();
+
+// Requiring the campground model
+const Campground = require('../models/campground');
+
+// Requiring the campgroundSchema which is a Joi Model
+const {campgroundSchema} = require('../models/joi models/schemas.js')
+
+
+// Requiring Error Utlilites 
+const ExpressError = require('../errorutlis/ExpressError');
+const catchAsync   = require('../errorutlis/catchAsync');
+
+
+/*Validating the campground before campground is even created using JOI validateCampground is a middleware which will be applied to put and post requests
+ campgroundSchema is in the file schemas.js
+*/
+const validateCampground =(req,res,next)=>{ 
+    const {error} = campgroundSchema.validate(req.body);
+    if(error){
+    const msg = error.details.map(el=> el.message).join(',');
+    throw new ExpressError(msg,400);
+    }
+    else{
+        next();
+    }
+}
+
+// Route which displays all the campgrounds
+router.get('/', catchAsync(async (req,res)=>{
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index',{campgrounds});
+}))
+
+
+// Route to create a new Campground
+router.get('/new',(req,res)=>{
+    res.render('campgrounds/new');
+})
+
+/* Route for the post request in creating the campground and then redirecting to show page 
+/campgrounds/:id--> show the details of campground
+validating the review  before review is even created using validateCampground Middleware which uses JOI
+*/
+router.post('/new',validateCampground,catchAsync(async (req,res,next)=>{
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
+    
+    // Validating the req.body using JOI before even creating the campground
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+    
+}))
+
+/*
+ Route which displays the details of a campground
+ And also display the reviews for each campground by populating the reviews
+*/
+ router.get('/:id', catchAsync(async(req,res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findById(id).populate('reviews');
+    res.render('campgrounds/show',{campground});
+}))
+
+
+// Route which renders the form to edit a campground
+router.get('/:id/edit', catchAsync(async (req,res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit',{campground});
+}))
+
+
+// Route which actually update a campground
+router.put('/:id/edit', validateCampground,catchAsync(async (req,res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+
+/* Route which deletes a campground and then redirecting to campgrounds
+   And also deletes all the reviews which are associated to that campground by using
+   campgroundSchema.post('findoneAndDelete',(data)) post middleware in campground model
+ */
+router.delete('/:id', catchAsync(async (req,res)=>{
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds');
+}))
+
+module.exports = router;
