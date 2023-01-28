17a9fbe337c682fc7c72ad3ef1850260fee628b1
Bhargavi Thaduri <bhargavithaduri1@gmail.com>
2023-01-28 19:18:03 +0530

    Break the reviews routes /campgrounds/:id/reviews and add mergeParams to true to the router

--- app.js
+++ app.js
@@ -5,20 +5,13 @@ const mongoose = require('mongoose');
 
 // EJS Mate to allow app to use layouts and partials
 const ejsMate = require('ejs-mate');
-const {campgroundSchema,reviewSchema} = require('./models/joi models/schemas.js')
 
-// Requiring the campground model
-const Campground = require('./models/campground');
-const Review = require('./models/review')
-
+// Overrides post as put or delete requests
 const methodOverride = require('method-override');
 
-// Requiring Error Utlilites 
-const ExpressError = require('./errorutlis/ExpressError');
-const catchAsync   = require('./errorutlis/catchAsync');
-
+// Requiring the campground and reviews routes
 const campgroundRoute = require('./routes/campground')
-
+const reviewRoute = require('./routes/reviews')
 
 // Mongoose Connection
 mongoose.set('strictQuery',true);
@@ -40,63 +33,20 @@ app.use(express.urlencoded({extended:true}));
 // Overriding the method for put and delete requests
 app.use(methodOverride('_method'));
 
-/* 
-App to use ejsMate engine
-Creates reusable code that will meet our goal to reduce duplicating code. like using layouts,partials
-*/
+//App to use ejsMate engine Creates reusable code that will meet our goal to reduce duplicating code. like using layouts,partials
 app.engine('ejs',ejsMate)
-
-
-/*validating the review  before review is even created using JOI validateReview is a middleware which will be applied to put and post requests
- reviewSchema is in the file schemas.js
- This middleware should be applied to post and put request while creating the review
-*/
-const validateReview = (req,res,next)=>{
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el=> el.message).join(',');
-        throw new ExpressError(msg,400);
-        }
-        else{
-            next();
-        }
-}
 
 // Home Route
 app.get('/',(req,res)=>{
    res.render('home.ejs');
 })
 
+// campground routes
 app.use('/campgrounds',campgroundRoute)
 
-
-// ---------------------Reviews Routes----------------------
-
-/* ----------------Reviews post route-----------
- The form is on show page of a campground
- Finds the campground with the id(foundCampground), creates a review,
- adds that review to the reviews array of the foundCampground by pushing capground.reviews.push(review)
-*/
-app.post('/campgrounds/:id/reviews',validateReview,catchAsync(async(req,res)=>{
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-
-/* ----------------Delete a review-----------------
- Removing the review in review model and also in campground model (reviews array[])
- Delete a review in campground reviews array by using $pull:{reviews:reviewId} which pulls all the ids which matches to reviewId in reviews array
-*/
-app.delete('/campgrounds/:id/reviews/:reviewId',catchAsync(async(req,res)=>{
-    const {id,reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id,{ $pull:{reviews:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
+// review routes
+// If we want to access :id params in the reviews route file then set mergeParams:true in reviews.js file
+app.use('/campgrounds/:id/reviews',reviewRoute)
 
 
 // Any other route
---
+++ routes/reviews.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router({mergeParams:true});
+
+// Requiring the campground and reviews model
+const Campground = require('../models/campground');
+const Review = require('../models/review')
+
+
+// Requiring the campgroundSchema which is a Joi Model
+const {reviewSchema} = require('../models/joi models/schemas.js')
+
+
+// Requiring Error Utlilites 
+const ExpressError = require('../errorutlis/ExpressError');
+const catchAsync   = require('../errorutlis/catchAsync');
+
+/*
+validating the review  before review is even created using JOI validateReview is a middleware which will be applied to put and post requests
+ reviewSchema is in the file schemas.js
+ This middleware should be applied to post and put request while creating the review
+*/
+const validateReview = (req,res,next)=>{
+    const {error} = reviewSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el=> el.message).join(',');
+        throw new ExpressError(msg,400);
+        }
+        else{
+            next();
+        }
+}
+
+/* ----------------Reviews post route-----------
+ The form is on show page of a campground
+ Finds the campground with the id(foundCampground), creates a review,
+ adds that review to the reviews array of the foundCampground by pushing capground.reviews.push(review)
+*/
+router.post('/',validateReview,catchAsync(async(req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+
+/* ----------------Delete a review-----------------
+ Removing the review in review model and also in campground model (reviews array[])
+ Delete a review in campground reviews array by using $pull:{reviews:reviewId} which pulls all the ids which matches to reviewId in reviews array
+*/
+router.delete('/:reviewId',catchAsync(async(req,res)=>{
+    const {id,reviewId} = req.params;
+    await Campground.findByIdAndUpdate(id,{ $pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
+
+module.exports = router;
