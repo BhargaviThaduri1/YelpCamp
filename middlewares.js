6de58dc2b4a5a5fe6b9882ff5e3dc70f7c397f71
Bhargavi Thaduri <bhargavithaduri1@gmail.com>
2023-02-04 11:50:10 +0530

    Add return to behaviour in middleware

--- middlewares.js
+++ middlewares.js
@@ -1,6 +1,9 @@
 module.exports.isLoggedIn = (req,res,next)=>{
     console.log('Req.user',req.user)
     if(!req.isAuthenticated()){
+        req.session.returnTo = req.originalUrl;
+        console.log(req.session.returnTo)
+        console.log(req.originalUrl)
         req.flash('error','You must be logged in!')
         return res.redirect('/login');
     }
--- routes/users.js
+++ routes/users.js
@@ -30,11 +30,11 @@ router.get('/login',(req,res)=>{
     res.render('users/login');
 })
 
-router.post('/login',passport.authenticate('local',{failureFlash:true,failureRedirect:'/login',successFlash:true}),(req,res)=>{
-    console.log("Successfully loggedIn")
-    console.log("Here is the user",req.user.username)
+router.post('/login',passport.authenticate('local',{failureFlash:true,failureRedirect:'/login',successFlash:true,    keepSessionInfo: true,
+}),(req,res)=>{
     req.flash('success','Welcome Back!!')
-    res.redirect('/campgrounds');
+    const redirectUrl = req.session.returnTo || 'campgrounds'
+    res.redirect(redirectUrl);
 })
 
 router.get('/logout',(req,res)=>{
