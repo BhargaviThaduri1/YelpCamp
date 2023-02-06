163a6a704556e08081fc05921e43cd6c08e02a91
Bhargavi Thaduri <bhargavithaduri1@gmail.com>
2023-02-06 10:35:37 +0530

    Add Reviews Controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router({mergeParams:true});
 
-// Requiring the campground and reviews model
-const Campground = require('../models/campground');
-const Review = require('../models/review')
-
+const Reviews = require('../controllers/reviews')
 const catchAsync   = require('../errorutlis/catchAsync');
 const {validateReview, isLoggedIn,isReviewAuthor} =require('../middlewares')
 
@@ -15,27 +12,12 @@ const {validateReview, isLoggedIn,isReviewAuthor} =require('../middlewares')
  Finds the campground with the id(foundCampground), creates a review,
  adds that review to the reviews array of the foundCampground by pushing capground.reviews.push(review)
 */
-router.post('/',isLoggedIn,validateReview,catchAsync(async(req,res)=>{
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    review.author = req.user._id;
-    await review.save();
-    await campground.save();
-    req.flash('success','Successfully created a new Review!!')
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.post('/',isLoggedIn,validateReview,catchAsync(Reviews.createReview))
 
 /* ----------------Delete a review-----------------
  Removing the review in review model and also in campground model (reviews array[])
  Delete a review in campground reviews array by using $pull:{reviews:reviewId} which pulls all the ids which matches to reviewId in reviews array
 */
-router.delete('/:reviewId',isLoggedIn,isReviewAuthor,catchAsync(async(req,res)=>{
-    const {id,reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id,{ $pull:{reviews:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success','Successfully Deleted Review!!')
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.delete('/:reviewId',isLoggedIn,isReviewAuthor,catchAsync(Reviews.deleteReview))
 
 module.exports = router;
