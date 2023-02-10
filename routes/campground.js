661a05d03e73fe40fc8fd330c0d9b81bf64973e0
Bhargavi Thaduri <bhargavithaduri1@gmail.com>
2023-02-10 20:52:07 +0530

    Write add and delete images options on campground Images

--- routes/campground.js
+++ routes/campground.js
@@ -6,6 +6,7 @@ const {isLoggedIn,isAuthor,validateCampground} = require('../middlewares')
 const multer = require('multer');
 
 const {storage,cloudinary} = require('../cloudinary/index');
+const Campground = require('../models/campground');
 // Store using our cloudinary storage i.e storage
 const upload = multer({storage});
 
@@ -23,6 +24,39 @@ router.route('/:id')
 router.route('/:id/edit')
     .get(isLoggedIn, isAuthor,catchAsync(campgrounds.renderEditForm))
     .put(isLoggedIn,isAuthor,validateCampground,catchAsync(campgrounds.updateCampground))
+
+router.route('/:id/images')
+    .get(async(req,res)=>{
+        const {id} = req.params;
+        const campground = await Campground.findById(id);
+        res.render('images/show',{campground});   
+    })
+    .post(upload.array('image'),catchAsync(async(req,res)=>{
+        const {id} = req.params;
+        const campground = await Campground.findById(id);
+        const imgs = req.files.map(f=>({url:f.path,filename:f.filename}));
+        campground.images.push(...imgs);
+        await campground.save();
+        req.flash('success','Successfully Uploaded the Image(s)');
+        res.redirect(`/campgrounds/${campground._id}`);
+    }))
+    .delete(catchAsync(async(req,res)=>{
+        const {id} = req.params;
+        const campground = await Campground.findById(id);
+        // We have access to images which checked via req.body
+        console.log(req.body);
+        if(req.body.deleteImages){
+            // Deleting Images in Cloudinary
+            for(let filename of req.body.deleteImages){
+               await cloudinary.uploader.destroy(filename);
+            }
+            // Deleting Images in Mongo
+            await campground.updateOne({$pull:{images:{filename:{$in: req.body.deleteImages}}}});
+            req.flash('success','Successfully deleted Image(s)')
+            res.redirect(`/campgrounds/${campground._id}`);
+        }
+        
+    }))
 
 
 module.exports = router;
