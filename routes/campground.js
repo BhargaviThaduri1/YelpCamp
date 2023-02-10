61c1187f8c4a7e9c3447a171c5773f742dda0455
Bhargavi Thaduri <bhargavithaduri1@gmail.com>
2023-02-10 10:47:12 +0530

    Use multer middleware for processing single and mutliple files by using req.files/req.file

--- routes/campground.js
+++ routes/campground.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const campgrounds = require('../controllers/campgrounds')
-const Campground = require('../models/campground');
 const catchAsync   = require('../errorutlis/catchAsync');
-
 const {isLoggedIn,isAuthor,validateCampground} = require('../middlewares')
-
+const multer = require('multer');
+const upload = multer({dest:'uploads/'});
 
 router.get('/', catchAsync(campgrounds.index))
 
 router.route('/new')
     .get(isLoggedIn,campgrounds.renderNewForm)
-    .post(isLoggedIn,validateCampground,catchAsync(campgrounds.createCampground))
-
-
+    // .post(isLoggedIn,validateCampground,catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'),(req,res)=>{
+            console.log(req.body)
+            console.log(req.files)
+    })
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
     .delete(isLoggedIn,isAuthor, catchAsync(campgrounds.deleteCampground))
