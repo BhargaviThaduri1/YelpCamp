18c526324cd0ad79d7cbc974d105336155b1d4fd
Bhargavi Thaduri <bhargavithaduri1@gmail.com>
2023-02-11 11:52:01 +0530

    Adding thumbnail virtual property to Images

--- models/campground.js
+++ models/campground.js
@@ -3,14 +3,18 @@ const Review = require('./review')
 
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url:String,
+    filename:String
+})
+
+// WHENEVER WE CALL THUMBNAIL ON CAMPGROUND.IMAGES.THUMBNAIL THEN VIRTUAL AUTOMATICALLY ADDS W_400 WHICH IS THE WIDTH/HEIGHT OF THE IMAGE
+ImageSchema.virtual('thumbnail').get(function(){
+    return this.url.replace('/upload', '/upload/w_300,h_300');
+})
 const CampgroundSchema = new Schema({
     title:String,
-    images:[
-        {
-            url:String,
-            filename:String
-        }
-    ],
+    images:[ImageSchema],
     price:Number,
     description:String,
     location:String,
