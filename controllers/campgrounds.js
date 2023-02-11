6e68d233503a1cbecc78d9e6a77aaa3087e514f2
Bhargavi Thaduri <bhargavithaduri1@gmail.com>
2023-02-11 20:42:08 +0530

    Geocoding the campground locations using mapbox geocoder

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,8 @@
-const Campground = require('../models/campground')
+const Campground = require('../models/campground');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geoCoder = mbxGeocoding({accessToken:mapBoxToken});
+
 module.exports.index = async (req,res)=>{
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index',{campgrounds});
@@ -7,13 +11,20 @@ module.exports.renderNewForm = (req,res)=>{
     res.render('campgrounds/new');
 }
 module.exports.createCampground = async (req,res,next)=>{
-    const campground = new Campground(req.body.campground);
-    campground.author = req.user._id;
-    // We have access to req.files=> which gives all the files which uploaded
-    campground.images = req.files.map(f=>({url:f.path,filename:f.filename}))
-    await campground.save();
-    req.flash('success','Successfully made a new campground!!')
-    res.redirect(`/campgrounds/${campground._id}`);
+    // GEOCODING THE LOCATIONS IT GIVES 1 RESULT AND THE LATTITUE AND LONGITUDE WILL BE IN GEOMETRY OBJECT.HENCE SELECTING FEATURES[0].GEOMETRY
+    const geoData = await geoCoder.forwardGeocode({
+        query:'Telangana,India',
+        limit:1
+    }).send()
+    console.log(geoData.body.features[0].geometry);
+    res.send('okk');
+    // const campground = new Campground(req.body.campground);
+    // campground.author = req.user._id;
+    // // We have access to req.files=> which gives all the files which uploaded
+    // campground.images = req.files.map(f=>({url:f.path,filename:f.filename}))
+    // await campground.save();
+    // req.flash('success','Successfully made a new campground!!')
+    // res.redirect(`/campgrounds/${campground._id}`);
     
 }
 module.exports.showCampground = async(req,res)=>{
